6966f5e2da57b0313169d093715fe8858eb3b0f1
bottinolucas <lucasgabottino@gmail.com>
2024-10-01 18:58:49 -0300

    Modelando TODO App

---
+++ Models/Todo.cs
@@ -0,0 +1,10 @@
+namespace API.Models
+{
+  public class Todo
+  {
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public bool IsDone { get; set; }
+    public DateTime CreatedAt { get; set; }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,31 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet(
-  "/{nome}", (string nome) =>
-  {
-    return Results.Ok($"Hello {nome}!");
-  }
-);
-
-app.MapGet(
-  "/name/{nome}", (string nome) =>
-  {
-    return Results.Ok($"Hello {nome}, welcome to my api!");
-  }
-);
-
-app.MapPost(
-  "/", (User user) =>
-  {
-    return Results.Ok(user);
-  }
-);
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
-
-public class User
-{
-  public int Id { get; set; }
-  public string Username { get; set; }
-}
