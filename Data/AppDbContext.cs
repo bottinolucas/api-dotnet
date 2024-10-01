723f87a39eeff62190bd418fab6767c26c36edd8
bottinolucas <lucasgabottino@gmail.com>
2024-10-01 19:05:44 -0300

    Adicionando DbContext, migrations com EF e criando base de dados

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+  public class AppDbContext : DbContext
+  {
+    public DbSet<Todo> Todos { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+      => optionsBuilder.UseSqlite("DataSource=app.db;Cache=Shared");
+
+  }
+}
